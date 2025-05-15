1de3cc3e99c40c2788cbf18eb7d52d7558f43a8e
wangxh <wangxh@koal.com>
2025-05-15 17:55:03 +0800

    feat: 界面设计

--- KeygenUi.py
+++ KeygenUi.py
@@ -23,7 +23,22 @@ class MainFrame ( wx.Frame ):
         wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+        self.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+        self.SetBackgroundColour( wx.Colour( 240, 240, 240 ) )
 
+        bSizer4 = wx.BoxSizer( wx.VERTICAL )
+
+        bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_filePicker1 = wx.FilePickerCtrl( self, wx.ID_ANY, wx.EmptyString, _(u"Select a file"), _(u"*.*"), wx.DefaultPosition, wx.DefaultSize, wx.FLP_DEFAULT_STYLE )
+        bSizer5.Add( self.m_filePicker1, 0, wx.ALL, 5 )
+
+
+        bSizer4.Add( bSizer5, 1, wx.EXPAND, 5 )
+
+
+        self.SetSizer( bSizer4 )
+        self.Layout()
 
         self.Centre( wx.BOTH )
 
